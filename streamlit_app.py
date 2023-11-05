523360c6d57aaf0d54897411425b3e793fb91fea
albanet5 <149885758+albanet5@users.noreply.github.com>
2023-11-05 09:33:13 -0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.header('At Orca, Stay :blue[Informed] Anywhere at Anytime', divider='green')
+
+st.subheader('Our Mission Statment')
+
+st.write("At Orca our mission is to provide you with timely and accurate weather information. We are committed to delivering precise data to ensure you stay informed about weather conditions. ")
+
+st.write("In recent times, there have been several instances where government weather alerts arrived too late. For example, during the recent tragedy in Acapulco, Mexico, many people were unaware of a Category 5 hurricane approaching. These delayed responses by governments, not only in Mexico but also in the United States, underscore the need for more accurate and real-time data provision to residents, enabling them to make informed decisions regarding their safety.")
+
+st.subheader('Put Your Life First!')
+
+st.write("The Orca Weather App offers a delightful and interactive approach to exploring weather conditions. Say goodbye to unexpected thunderstorms and stay well-informed about today's climate. Our app harnesses real-time data from trusted sources, presenting it in a user-friendly format. Whether it's humidity levels or any other weather detail, we've got you covered.")
+
+st.subheader("We deliever Real-life Solution for those who need it the most")
+
+st.write("We aim to not only provide an exceptional app with precise features but also to create a complementary product. Our endeavor involves designing a light-up prototype in the form of a water droplet, which will assist individuals engaged in daily activities by alerting them to impending rain before they venture outside. Typically, people rely on a quick look outside for weather information, which can often be inaccurate. Additionally, this product will emit a brief alerting sound when it's raining, aiding visually impaired individuals in recognizing the rain and taking necessary precautions when heading outdoors.")
