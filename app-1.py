e9bdb9f380ac0015b7e518b7bbe2db220af8e558
albanet5 <149885758+albanet5@users.noreply.github.com>
2023-11-05 09:33:50 -0500

    Add files via upload

---
+++ app-1.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.header('At Orca, Stay :blue[Informed] Anywhere at Anytime', divider='green')
+
+st.subheader('Our Mission Statment')
+
+st.write("At Orca our mission is to provide you with timely and accurate weather information. We are committed to delivering precise data to ensure you stay informed about weather conditions. ")
+
+st.write("In recent times, there have been several instances where government weather alerts arrived too late. For example, during the recent tragedy in Acapulco, Mexico, many people were unaware of a Category 5 hurricane approaching. These delayed responses by governments, not only in Mexico but also in the United States, underscore the need for more accurate and real-time data provision to residents, enabling them to make informed decisions regarding their safety.")
+
+st.subheader('Put Your Life First!')
+
+st.write("The Orca Weather App offers a delightful and interactive approach to exploring weather conditions. Say goodbye to unexpected thunderstorms and stay well-informed about today's climate. Our app harnesses real-time data from trusted sources, presenting it in a user-friendly format. Whether it's humidity levels or any other weather detail, we've got you covered.")
+
